cf729bb6dffc4d69e340d56bda048014ef007c76
Renan Lisboa <renlp89.developer@gmail.com>
2020-11-16 14:33:33 -0300

    webpack added

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import api from './services/api';
 
-import "./styles.css";
+import "./App.css";
 
 function App() {
   const [ repositories, setRepositories ] = useState([]);
